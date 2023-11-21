636d04e6568517754102ced6b4c9ed34f5621f43
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-21 15:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 //your JS code here. If required.
+let first=document.querySelector(".first");
+let one = document.getElementById("one");
+let stop= document.querySelector(".stop")
+
+let second=document.querySelector(".second");
+let two = document.getElementById("two");
+
+let third=document.querySelector(".third");
+let three = document.getElementById("three");
+
+let forth=document.querySelector(".forth");
+let four = document.getElementById("four");
+
+let fifth=document.querySelector(".fifth");
+let five = document.getElementById("five");
+
+let sixth=document.querySelector(".sixth");
+let six = document.getElementById("six");
+
+stop.addEventListener("click",()=>{
+    one.pause();
+    two.pause();
+    three.pause();
+    four.pause();
+    five.pause();
+    six.pause();
+    
+})
+
+first.addEventListener("click",()=>{
+    console.log("working");
+    one.play();
+})
+
+second.addEventListener("click",()=>{
+    console.log("working");
+    two.play();
+})
+third.addEventListener("click",()=>{
+    console.log("working");
+    three.play();
+})
+forth.addEventListener("click",()=>{
+    console.log("working");
+    four.play();
+})
+fifth.addEventListener("click",()=>{
+    console.log("working");
+    five.play();
+})
+sixth.addEventListener("click",()=>{
+    console.log("working");
+    six.play();
+})
